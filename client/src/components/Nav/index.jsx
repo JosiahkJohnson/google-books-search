81ea1b2c3711e93ef7e5f9419a14ab0d180e22fe
Josiah <josiahkj123@gmail.com>
2020-06-09 00:02:25 -0500

    Layed the groundwork for the front end of this react application

---
+++ client/src/components/Nav/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Nav() {
+    return(
+        <nav className = "navbar navbar-expand-lg navbar-light bg-light">
+            <a className = "navbar-brand" href="/">Book Reader</a>
+        </nav>
+    );
+};
+
+export default Nav;
---
+++ client/src/utilities/googleDatabase.js
@@ -0,0 +1,14 @@
+import api from "../api";
+
+//functions to interact with the database
+export default  {
+    searchTitle: function(title) {
+        const query = title;
+      
+        api.byTitle(query).then(response => {
+          return response;
+        }).catch(error => {
+          console.log(error);
+        });
+      };
+}
